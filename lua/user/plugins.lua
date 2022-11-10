eedd77bd4b9ff122c674d910781352f2163e2009
Huabei You <huabeiyou@gmail.com>
2022-11-09 22:58:22 -0500

    dial setup

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -9,40 +9,10 @@ lvim.plugins = {
 	{ "f-person/git-blame.nvim" },
 	{ "tpope/vim-surround" },
 	{ "tpope/vim-repeat" },
-	{
-		"monaqa/dial.nvim",
-		event = "BufRead",
-		config = function()
-			local dial = require("dial")
-			vim.cmd([[
-      nmap <C-a> <Plug>(dial-increment)
-      nmap <C-x> <Plug>(dial-decrement)
-      vmap <C-a> <Plug>(dial-increment)
-      vmap <C-x> <Plug>(dial-decrement)
-      vmap g<C-a> <Plug>(dial-increment-additional)
-      vmap g<C-x> <Plug>(dial-decrement-additional)
-    ]])
-			dial.augends["custom#boolean"] = dial.common.enum_cyclic({
-				name = "boolean",
-				strlist = { "true", "false" },
-			})
-			table.insert(dial.config.searchlist.normal, "custom#boolean")
-
-			-- For Languages which prefer True/False, e.g. python.
-			dial.augends["custom#Boolean"] = dial.common.enum_cyclic({
-				name = "Boolean",
-				strlist = { "True", "False" },
-			})
-			table.insert(dial.config.searchlist.normal, "custom#Boolean")
-		end,
-	},
+	{ "monaqa/dial.nvim" },
 	{ "windwp/nvim-ts-autotag" },
 	{ "p00f/nvim-ts-rainbow" },
 	{ "folke/todo-comments.nvim" },
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		event = "BufRead",
-	},
 	{
 		"norcalli/nvim-colorizer.lua",
 		config = function()
